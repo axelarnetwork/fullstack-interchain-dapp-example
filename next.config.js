42208eee8840801b6f09bd0fbd09eefdc5d64bd3
Olanetsoft <heedris2olubisi@gmail.com>
2023-06-06 13:13:07 +0100

    fix: invalid next.config warning

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  future: {
-    webpack5: true,
-  },
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback,
