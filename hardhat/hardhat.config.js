7711fe4a74e13cfd0207b89826c73dbe20a8f7ef
Olanetsoft <heedris2olubisi@gmail.com>
2023-06-07 13:59:08 +0100

    chore: add crosschain smart contract

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,38 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+require("solidity-coverage");
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    bsc: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: [PRIVATE_KEY],
+    },
+    avalancheFujiTestnet: {
+      url: "https://avalanche-fuji-c-chain.publicnode.com",
+      chainId: 43113,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  mocha: {
+    timeout: 10000000000,
+  },
+};
